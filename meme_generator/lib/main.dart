956a4f8b4b4d7b4f9297b9d68ca255ea09ed8366
Ginger_mustache <artzaneg@gmail.com>
2024-03-07 11:55:42 +0300

    rewrite app creater

--- meme_generator/lib/main.dart
+++ meme_generator/lib/main.dart
@@ -2,16 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:meme_generator/di_container/di_container.dart';
 import 'package:meme_generator/i18n/strings.g.dart';
 
-abstract class AppFactory {
-  Widget makeApp();
-}
-
-final appFactory = makeAppFactory();
-
 void main() async {
+  final DiContainerProvider diContainer = DiContainer();
   WidgetsFlutterBinding.ensureInitialized();
-  final app = appFactory.makeApp();
-  initStores();
+  final app = diContainer.makeApp();
+  initStores(diContainer);
 
   runApp(TranslationProvider(child: app));
 }
